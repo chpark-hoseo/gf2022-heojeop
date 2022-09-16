07a635f1195463e396180defb538f2f251674140
heojeop <20191246@vision.hoseo.edu>
2022-09-16 17:56:25 +0900

    HelloSDL분리하기-실습
    게임 실행 구조
    
    main 함수에 몰려있던 소스코드를
    init , render 함수로 분리

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,42 @@ SDL_Window*		g_pWindow = 0;
 SDL_Renderer*	g_pRenderer = 0;
 bool g_bRunning = false; //게임 실행시 참일경우 동작
 
+//창 제목, x위치, y위치, 좌표에서 창의 너비, 좌표에서 창의 높이, 0또는 하나 이상의 Flags)
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+        g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
+
+        // g_pWindow 가 0이 아닐경우 Renderer 초기화
+        if (g_pWindow != 0) {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+        }
+    }
+    else {
+        return false;
+    }
+    // RGB 값으로 색상을 설정하는 함수 ( Red, Green, Blue, 투명도)
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+    SDL_Delay(1000);
+
+    return true;
+}
+
+void render() {
+
+    //백버퍼 그리기
+    SDL_RenderClear(g_pRenderer);
+
+    //메인버퍼 출력
+    SDL_RenderPresent(g_pRenderer);
+
+}
+
 int main(int argc, char* args[]){
     //init 함수 호출
     if (init("Breaking Up HelloSDL",
              SDL_WINDOWPOS_CENTERED,
-             SDL_WINDOWPOS_CENTERED, 640, 480,
-             SDL_WINDOW_SHOWN))
+             SDL_WINDOWPOS_CENTERED, 
+             640, 480,SDL_WINDOW_SHOWN))
     {
         g_bRunning = true;
         
@@ -24,39 +54,13 @@ int main(int argc, char* args[]){
         //render 함수 호출
         render();
     }
+    
     //SDL 종료
     SDL_Quit();
     return 0;
 }
 
-//창 제목, x위치, y위치, 좌표에서 창의 너비, 좌표에서 창의 높이, 0또는 하나 이상의 Flags)
-bool init(const char* title, int xpos, int height, int width, int flags){
-    if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
-        g_pWindow = SDL_CreateWindow(title, xpos, height, width, flags);
-        
-        // g_pWindow 가 0이 아닐경우 Renderer 초기화
-        if(g_pWindow != 0){
-            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
-    }else{
-        return false;
-    }
-    // RGB 값으로 색상을 설정하는 함수 ( Red, Green, Blue, 투명도중 rand 함수를 통해 랜덤으로 교체)
-    SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand % 256, rand() % 256, 255;);
-    SDL_Delay(1000)
-    
-    return true;
-}
 
-void render(){
-    
-    //백버퍼 그리기
-    SDL_RenderClear(g_pRenderer);
-    
-    //메인버퍼 출력
-    SDL_RenderPresent(g_pRenderer);
-    
-}
 
 
 
